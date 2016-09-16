74190cc48c8ec3a9130884e318610b72592566cc
Anthony Michelizzi <cynical89@gmail.com>
2016-09-16 11:44:59 -0700

    Added tests, updated user

--- models/users.js
+++ models/users.js
@@ -6,11 +6,13 @@ const chance = new Chance();
 module.exports = {
 	newUser: (username, password, level) => {
 		const user = {
+			error: false,
 			id: chance.hammertime(),
 			username: username,
 			password: password,
 			// is this an account for the store or consumers?
-			storeAccount: false
+			storeAccount: false,
+			authLevel: 0
 		};
 	}
 };
---
+++ tests/users.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const expect = require("chai").expect;
+const useModel = require("../models/order");
+
+let user;
+
+describe("User Model - New User", () => {
+	before(() => {
+		order = userModel.newUser("name", "password", 99);
+	});
+
+	it("user should be a valid object", (done) => {
+		expect(user).to.not.be.an("undefined");
+		expect(user).to.be.an("object");
+		return done();
+	});
+
+	it("user should have required properties", (done) => {
+		expect(user).to.have.property("error");
+		expect(user).to.have.property("id");
+		expect(user).to.have.property("username");
+		expect(user).to.have.property("password");
+		expect(user).to.have.property("storeAccount");
+		expect(user).to.have.property("authLevel");
+		return done();
+	});
+
+	it("user should have the correct starting values", (done) => {
+		expect(user.error).to.be.a("boolean");
+		expect(user.error).to.equal(false);
+		// guid with length of 36 - 32 chars 4 dashes
+		expect(user.id).to.be.a("string");
+		expect(user.id.length).to.equal(36);
+		expect(user.username).to.be.a("string");
+		expect(user.username).to.equal("name");
+		expect(user.password).to.be.a("string");
+		expect(user.password).to.equal("password");
+		expect(user.storeAccount).to.be.a("boolean");
+		expect(order.storeAccount).to.equal(false);
+		expect(order.level).to.be.a("number");
+		expect(order.level).to.equal(99);
+		return done();
+	});
+});
